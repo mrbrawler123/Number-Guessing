f28de8deadcdd5dfe1fc902871e436e6a944b752
mrbrawler <omkaar.nerurkar@gmail.com>
2021-02-23 05:45:11 +0000

    minor bug fixes

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ time.sleep(3)
 print("I will think of a number. You have to guess it.")
 time.sleep(3)
 x = 0
-n = int(input("Enter your number limit: "))
+n = input("Enter your number limit: ")
+while True:
+	try:
+		int(n)
+		break
+	except ValueError:
+		n = input("Only enter an integer value: ")
+n = int(n)
 import random
 r = random.randint(1,n)
 while True:
@@ -12,12 +19,22 @@ while True:
 		s = int(input("Enter your guess: "))
 		break
 	except ValueError:
-		print("Please input a number.")
+		print("Please input an ineger: ")
 while s != r:
 	if r > s:
-		s = int(input("Enter a higher guess: "))
+		while True:
+			try:
+				s = int(input("Enter a higher guess: "))
+				break
+			except ValueError:
+				s = input("Only enter an integer value: ")
 		x = x + 1
 	if r < s:
-		s = int(input("Enter a lower guess: "))
+		while True:
+			try:
+				s = int(input("Enter a lower guess: "))
+				break
+			except ValueError:
+				s = input("Only enter an integer value: ")
 		x = x + 1
 print(f"Congratulations! You guessed it in {x} tries!")
